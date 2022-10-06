749b8abfbecb8036cb17380ff8224290ad9e0153
igmazzini <igmazzini2013@gmail.com>
2022-10-06 10:32:43 -0300

    Twittor

--- sw.js
+++ sw.js
@@ -3,9 +3,9 @@ importScripts('js/sw-utils.js');
 const STATIC_CACHE      = 'static-v3';
 const DYNAMIC_CACHE     = 'dynamic-v1';
 const INMUTABLE_CACHE   = 'inmutable-v1';
-
+//'/',
 const APP_SHELL = [
-    //'/',
+    
     '/index.html',
     'css/style.css',    
     'img/favicon.ico',
